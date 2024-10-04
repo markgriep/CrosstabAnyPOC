4cefa97a24acce253fa4a5da437b286efc58a8c9
megriep <markgriep@users.noreply.github.com>
2024-10-04 12:31:26 -0500

    Add a simple mapping code

--- CrosstabAnyPOC/Program.cs
+++ CrosstabAnyPOC/Program.cs
@@ -9,9 +9,40 @@ namespace CrosstabAnyPOC
             var _mappings = JobToDepartmentMapping.GetMockMappings();
             var _employees = WorkDayEmployee.GetMockEmployees();
 
-            // Sanity checks
-            Utility.PrintMapping(_mappings);
-            Utility.PrintEmployees(_employees);
+
+
+
+
+
+            // Simple Match employees with the active mappings
+            var SelectionPool = _employees.Where(emp =>
+                _mappings.Any(map =>
+                    map.IsActive && // Only match with active mappings
+                    map.CostCenterID == emp.DepartmentID &&
+                    map.JobCodeID == emp.JobCode)).ToList();
+
+
+
+
+
+
+            // Print matched employees
+            Console.WriteLine("Matched Employees:");
+            foreach (var emp in SelectionPool)
+            {
+                Console.WriteLine($"{emp.Name} - DepartmentID: {emp.DepartmentID}, JobCode: {emp.JobCode}");
+            }
+
+
+
+
+
+
+
+
+            //// Sanity checks
+            //Utility.PrintMapping(_mappings);
+            //Utility.PrintEmployees(_employees);
 
             Console.ReadKey();
 
