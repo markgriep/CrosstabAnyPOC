ddae137c122eaca3a4bd189d92892ff8471e64c0
Mark Griep <noreply@foo.com>
2024-10-09 18:56:47 -0500

    waypoint of the night

--- CrosstabAnyPOC/Models/WorkDayEmployee.cs
+++ CrosstabAnyPOC/Models/WorkDayEmployee.cs
@@ -3,15 +3,17 @@
     //#########################################################################
     public class WorkDayEmployee
     {
-        public string Name { get; set; }      // Employee's Name
-        public int DepartmentID { get; set; } // Department ID (integer)
-        public string JobCode { get; set; }   // Job Code (alphanumeric)
+        public int EmployeeId { get; set; }      // EmployeeID (INTEGER)
+        public string Name { get; set; }            // Employee's Name
+        public int DepartmentID { get; set; }       // Department ID (integer)
+        public string JobCode { get; set; }         // Job Code (alphanumeric)
 
-        public WorkDayEmployee(string name, int departmentID, string jobCode)
+        public WorkDayEmployee(string name, int departmentID, string jobCode, int employeeId)
         {
             Name = name;
             DepartmentID = departmentID;
             JobCode = jobCode;
+            EmployeeId = employeeId;
         }
 
 
@@ -26,31 +28,47 @@
             return new List<WorkDayEmployee>
             {
                 //                    Name          DeptID       JobCode
-                new WorkDayEmployee("Alice",        990,           "001"),
-                new WorkDayEmployee("X-Bob",        990,           "009"),
-                new WorkDayEmployee("Charlie",      990,           "003"),
+                new WorkDayEmployee("Alice",        990,           "001",   890291),
+                new WorkDayEmployee("X-Bob",        990,           "009",   890291),
+                new WorkDayEmployee("Charlie",      990,           "003",   890291),
+                                                                           
+                new WorkDayEmployee("Diana",        119,           "002",   890291),
+                new WorkDayEmployee("Edward",       119,           "003",   890291),
+                new WorkDayEmployee("X-Fiona",      119,           "004",   890291),
+                new WorkDayEmployee("X-George",     119,           "009",   890291),
+                                                                           
+                new WorkDayEmployee("Hannah",       111,           "005",   890291),
+                new WorkDayEmployee("Ian",          111,           "005",   890291),
+                new WorkDayEmployee("Jonah",        111,           "005",   890291),
+                new WorkDayEmployee("Kevin",        111,           "005",   890291),
+                                                                           
+                new WorkDayEmployee("Lan",          005,           "022",   890291),
+                new WorkDayEmployee("X-Man",        005,           "029",   890291),
+                new WorkDayEmployee("X-Nud",        005,           "028",   890291),
 
-                new WorkDayEmployee("Diana",        119,           "002"),
-                new WorkDayEmployee("Edward",       119,           "003"),
-                new WorkDayEmployee("X-Fiona",      119,           "004"),
-                new WorkDayEmployee("X-George",     119,           "009"),
-
-                new WorkDayEmployee("Hannah",       111,           "005"),
-                new WorkDayEmployee("Ian",          111,           "005"),
-                new WorkDayEmployee("Jonah",        111,           "005"),
-                new WorkDayEmployee("Kevin",        111,           "005"),
-
-                new WorkDayEmployee("Lan",          005,           "022"),
-                new WorkDayEmployee("X-Man",        005,           "029"),
-                new WorkDayEmployee("X-Nud",        005,           "028"),
-                new WorkDayEmployee("Odd",          005,           "023")
+                
+                new WorkDayEmployee("Odd",          005,           "023",   890291),
             };
         }
+
+
+
+        TO DO
+            See if the code will generate employee ids
+
+            https://chatgpt.com/c/67055bf3-16b4-800e-9ce8-c449a6df4506
+
 
 
         public static List<WorkDayEmployee> GetMockEmployees(int numberOfEmployees)
         {
             List<WorkDayEmployee> employeeList = new List<WorkDayEmployee>();
+
+            // Generate unique employee IDs before the for loop
+            HashSet<int> employeeIDs = GenerateUniqueEmployeeIDs(numberOfEmployees);
+            int[] employeeIDsArray = employeeIDs.ToArray(); // Convert HashSet to array for indexed access
+
+
 
             for (int i = 0; i < numberOfEmployees; i++)
             {
@@ -60,8 +78,12 @@
                 // Alternate between random job code generators for variety
                 string jobCode = (i % 2 == 0) ? GetRandomJobCode() : GetRandomWeightedJobCode();
 
+
+                // Assign employee ID and create new WorkDayEmployee
+                int employeeID = employeeIDsArray[i];
+
                 // Create a new WorkDayEmployee and add to the list
-                employeeList.Add(new WorkDayEmployee(name, departmentID, jobCode));
+                employeeList.Add(new WorkDayEmployee(name, departmentID, jobCode, employeeID));
             }
 
             return employeeList;                                                        // Return the complete list of employees
@@ -122,6 +144,30 @@
             // Fallback in case something goes wrong (shouldn't happen with correct setup)
             return jobCodeWeights.Keys.First();                        // Return the first job code as fallback
         }
+
+
+        private static HashSet<int> GenerateUniqueEmployeeIDs(int count)
+        {
+            HashSet<int> employeeIDs = new HashSet<int>();
+            Random random = new Random();
+
+            while (employeeIDs.Count < count)
+            {
+                // Generate random value based on weighted probability
+                int randomValue = random.NextDouble() switch
+                {
+                    < 0.6 => random.Next(2000, 2251),      // 60% chance for range 2000-2250
+                    < 0.9 => random.Next(2251, 2501),      // 30% chance for range 2251-2500
+                    _ => random.Next(2501, 3000)           // 10% chance for range 2501-2999
+                };
+
+                int employeeID = 890000 + randomValue;     // Generate the final employee ID (e.g., 892345)
+                employeeIDs.Add(employeeID);               // Add to HashSet for uniqueness
+            }
+
+            return employeeIDs;
+        }
+
 
     }
 } // End of namespace CrosstabAnyPOC
